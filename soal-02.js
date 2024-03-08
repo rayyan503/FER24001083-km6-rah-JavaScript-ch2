702ba157050be29b0df5ea383c7234aa1fdaa276
Rahmat Arayyan <rahmatarayyan26@gmail.com>
2024-03-08 13:24:46 +0700

    Soal Pemprograman JavaScript

--- soal-02.js
+++ soal-02.js
@@ -1,15 +1,14 @@
 const checkTypeNumber = (givenNumber) => {
-  // Memeriksa apakah givenNumber merupakan angka
-  if (typeof givenNumber !== "number") {
+  // memeriksa data
+  if (givenNumber) {
+    if (typeof givenNumber === "number") {
+      return givenNumber % 2 === 0 ? "Genap" : "Ganjil";
+    }
     return "Error: Invalid data type";
+    // return Error("Invalid data type");
   }
-
-  // Menentukan givenNumber adalah genap atau ganjil
-  if (givenNumber % 2 === 0) {
-    return "GENAP";
-  } else {
-    return "GANJIL";
-  }
+  return "Error: Bro Where is the parameter";
+  // return Error("Bro Where is the parameter");
 };
 
 console.log(checkTypeNumber(10));
@@ -18,3 +17,11 @@ console.log(checkTypeNumber("3"));
 console.log(checkTypeNumber({}));
 console.log(checkTypeNumber([]));
 console.log(checkTypeNumber());
+
+// Ekspektasi Output :
+// => GENAP
+// => GANJIL
+// => Error: Invalid data type
+// => Error: Invalid data type
+// => Error: Invalid data type
+// => Error: Bro where is the parameter?
--- soal-04.js
+++ soal-04.js
@@ -1,27 +1,27 @@
 const dataPenjualanPakAldi = [
   {
     namaProduct: "Sepatu Futsal Nike Vapor Academy 8",
-    hargaSatuan: 760808,
+    hargaSatuan: 760000,
     kategori: "Sepatu Sport",
-    totalTerjual: 98,
+    totalTerjual: 90,
   },
   {
     namaProduct: "Sepatu Warrior Tristan Black Brown High - Original",
-    hargaSatuan: 960808,
+    hargaSatuan: 960000,
     kategori: "Sepatu Sneaker",
     totalTerjual: 37,
   },
   {
     namaProduct: "Sepatu Warrior Tristan Maroon High - Original",
-    hargaSatuan: 360808,
+    hargaSatuan: 360000,
     kategori: "Sepatu Sneaker",
-    totalTerjual: 98,
+    totalTerjual: 90,
   },
   {
     namaProduct: "Sepatu Warrior Rainbow Tosca Corduroy - [BNIB] Original",
-    hargaSatuan: 120808,
+    hargaSatuan: 120000,
     kategori: "Sepatu Sneaker",
-    totalTerjual: 98,
+    totalTerjual: 90,
   },
 ];
 
